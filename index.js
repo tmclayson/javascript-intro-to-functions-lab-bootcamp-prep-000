43397f7e745dae7b2b336b29701c5c8fff39966f
Tom Clayson <tmclayson@gmail.com>
2019-02-03 17:02:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return(`I can't here you!`);
+    return(`I can't hear you!`);
   }
   if (string === string.toUpperCase()) {
     return(`YES INDEED!`);
   }
   if (string === `I love you, Grandma.`) {
-    return(`I love you, too`);
+    return(`I love you, too.`);
   }
 }
